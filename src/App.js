de30a49253025cb3c98286962d26847ddae4096a
Anton <enitefall81@gmail.com>
2021-05-17 11:35:26 -0700

    Fixed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, {useState} from "react"
 import * as Y from "yup"
 
-export default function CreateForm() {
-  let initialInputs = {
-  username: "",
-  email: "",
-  about: "..."
+export default function EditProfileForm() {
+  // Initial data (from fetch, etc.)
+  let profile = {
+    username: "john-snow",
+    email: "john-snow@gmail.com",
+    about: "...",
   }
-
-  let [inputs, setInputs] = useState(initialInputs)
-  let [errors, setErrors] = useState()
-  let [busy, setBusy] = useState()
-
-    async function onChange(event) {
+  let [inputs, setInputs] = useState({
+    username: profile.username,
+    email: profile.email,
+    about: profile.about,
+  })
+  let [errors, setErrors] = useState({})
+  let [busy, setBusy] = useState(false)
+  async function onChange(event) {
     let {target: {type, name, value, checked}} = event
     value = type == "checkbox" ? checked : value
     // Validation here delays visual feedback but minimizes the # of DOM updates
@@ -22,34 +25,41 @@ export default function CreateForm() {
     setInputs(inputs => ({...inputs, [name]: value}))
     setErrors({...errors, ...inputErrors})
   }
-    async function onSubmit(event) {
-    event.preventDefault()
-    let errors = await schema.validate(inputs, {abortEarly: false})
-      .then(_ => ({}))
-      .catch(convert)
-    setErrors(errors)
-    if (!Object.keys(errors).length) {
-      console.log(inputs)
-    } else {
-      console.log(errors)
+  function onSubmit(afterSubmit) {
+    return async function (event) {
+      event.preventDefault()
+      setBusy(true)
+      let errors = await schema.validate(inputs, {abortEarly: false})
+        .then(_ => ({}))
+        .catch(convert)
+      setErrors(errors)
+      if (Object.keys(errors).length) {
+        setBusy(false)
+      } else {
+        await afterSubmit(inputs)
+        setBusy(false)
+      }
     }
   }
-
+  async function handleSubmit(inputs) {
+    await delay(500)
+    console.log(inputs)
+  }
   return <div className="p-3">
     <h1 className="h3 mb-3">Edit Profile</h1>
     <form autoComplete="off"
           style={{maxWidth: "800px"}}
-          onSubmit={onSubmit}>
+          onSubmit={onSubmit(handleSubmit)}>
       <div className="form-group">
-        <label>Username</label> ()<br/>
+        <label>Username</label> ({errors.username || "*"})<br/>
         <input name="username"
                className="form-control"
                type="text"
                value={inputs.username}
                onChange={onChange}/>
       </div>
-       <div className="form-group">
-        <label>Email</label> ()<br/>
+      <div className="form-group">
+        <label>Email</label> ({errors.email || "*"})<br/>
         <input name="email"
                className="form-control"
                type="email"
@@ -57,33 +67,32 @@ export default function CreateForm() {
                onChange={onChange}/>
       </div>
       <div className="form-group">
-    <label>About</label> ()<br/>
-    <textarea name="about"
-      rows={5}
-      className="form-control"
-      value={inputs.about}
-      onChange={onChange}/>
-  </div>
+        <label>About</label> ({errors.about || "*"})<br/>
+        <textarea name="about"
+                  className="form-control"
+                  type="text"
+                  value={inputs.about}
+                  onChange={onChange}
+                  rows={6}/>
+      </div>
       <div>
         <button className="btn btn-primary"
-                disabled={false}>
-          Save
+                disabled={busy}>
+          Save {busy && <i className="fa fa-spinner fa-pulse fa-1x fa-fw"></i>}
         </button>
       </div>
     </form>
   </div>
-  }
-
-
+}
 let schema = Y.object().shape({
   username: Y.string().required()
+    .min(3).max(50)
+    .matches(/^[-a-z0-9]*$/),
+  email: Y.string().email().required()
     .min(3).max(50),
-  email: Y.string().required()
-    .min(3).max(500),
   about: Y.string().required()
-      .min(3).max(500).required()
+    .min(3).max(500),
 })
-
 let convert = (errors) => {
   return errors.inner.reduce((z, item) => {
     let name = (item.path || "").includes(".")
@@ -91,4 +100,9 @@ let convert = (errors) => {
       : item.path || ""
     return z[item.path || ""] ? z : {...z, [name]: item.message}
   }, {})
+}
+function delay(ms) {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, ms)
+  })
 }
